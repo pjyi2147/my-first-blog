0475e4178637879f84728d837c249746c4e93762
yjs990427 <yjs990427@gmail.com>
2016-09-07 22:38:24 -0400

    시간순정렬 최신이 위로 오게끔

--- blog/views.py
+++ blog/views.py
@@ -5,5 +5,5 @@ from .models import Post
 from django.utils import timezone
 
 def post_list(request):
-    posts = Post.objects.filter().order_by('published_date') #필터에 넣으면 안나옴... 왜지...?
+    posts = Post.objects.filter().order_by('-published_date') #필터에 넣으면 안나옴... 왜지...?
     return render(request, 'blog/post_list.html', {'posts': posts})
