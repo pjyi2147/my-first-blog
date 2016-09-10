8e608a78f0d9dcfc2424634b61b3a19f83727484
yjs990427 <yjs990427@gmail.com>
2016-09-10 19:34:27 -0400

    django_forms/ edit form & new form

--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,41 @@ from django.shortcuts import render
 # Create your views here.
 from .models import Post
 from django.utils import timezone
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import PostForm
+
 
 def post_list(request):
-    posts = Post.objects.filter().order_by('-published_date') #필터에 넣으면 안나옴... 왜지...?
+    posts = Post.objects.filter().order_by('-published_date') #필터에 created_date 같은거 넣으면 안나옴... 왜지...?
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html',{'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
